3551df30cafac258eef6d63b96b8dcfa3a797fc4
Simons102 <ssievert56@gmail.com>
2020-12-03 10:24:40 +0100

    add license

--- wpa2.sh
+++ wpa2.sh
@@ -1,4 +1,22 @@
 #!/system/bin/sh
+
+#   SkySecure - allows you to set a WiFi password for the Parrot SkyController 1
+#   Copyright (C) 2020  Simon Sievert
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 INIT_ACCESS_POINT="/system/xbin/initAccessPoint"
 INIT_ACCESS_POINT_BACKUP="/system/xbin/initAccessPoint.bak"
 DEFAULT_PASSWORD="skycontroller" # such security
